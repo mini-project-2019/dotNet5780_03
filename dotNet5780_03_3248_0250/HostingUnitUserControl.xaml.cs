1d6db1ffafe9e674b10364ef5fd84337f71c6116
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2019-12-15 18:18:10 +0200

    Made level 9

--- dotNet5780_03_3248_0250/HostingUnitUserControl.xaml.cs
+++ dotNet5780_03_3248_0250/HostingUnitUserControl.xaml.cs
@@ -21,11 +21,36 @@ namespace dotNet5780_03_3248_0250
     public partial class HostingUnitUserControl : UserControl
     {
         public HostingUnit CurrentHostingUnit;
+        private Calendar MyCalendar;
         public HostingUnitUserControl(HostingUnit hostingUnit)
         {
             InitializeComponent();
             this.CurrentHostingUnit = hostingUnit;
             UserControlGrid.DataContext = hostingUnit;
+            MyCalendar = CreateCalendar();
+            vbCalendar.Child = null; // מחיקה מהתצוגה של החלון הקודם
+            vbCalendar.Child = MyCalendar; // הצגה של החלון הנוכחי שיצרנו
+            SetBlackOutDates(); 
         }
+
+        private Calendar CreateCalendar()
+        {
+            Calendar MonthlyCalendar = new Calendar();
+            MonthlyCalendar.Name = "MonthlyCalendar";
+            MonthlyCalendar.DisplayMode = CalendarMode.Month;
+            MonthlyCalendar.SelectionMode = CalendarSelectionMode.SingleRange;
+            MonthlyCalendar.IsTodayHighlighted = true;
+            return MonthlyCalendar;
+        }
+
+        private void SetBlackOutDates()
+        {
+            foreach (DateTime date in CurrentHostingUnit.AllOrders)
+            {
+                MyCalendar.BlackoutDates.Add(new CalendarDateRange(date));
+            }
+        }
+
+
     }
 }
